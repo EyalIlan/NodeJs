832442752b8f3f9938b227721727d1f7c904efc9
Your Name <you@example.com>
2021-04-11 13:40:28 +0300

    first commit

---
+++ 1.1-node vs js/main.js
@@ -0,0 +1,4 @@
+
+// 1.NodeJS is a Javascript runtime environment.  Javascript can only be run in the browsers.
+// 2 NodeJS code can be run outside the browser.  Javascript can only be run in the browsers.  
+// 3 It is mostly used on the server-side. 	       It is basically used on the client-side.
