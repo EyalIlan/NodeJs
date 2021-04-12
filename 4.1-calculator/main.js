d74768abb3d783ca6e013b49304320b22049f7e7
Your Name <you@example.com>
2021-04-12 09:35:12 +0300

    ex4.1

---
+++ 4.1-calculator/main.js
@@ -0,0 +1,37 @@
+const yargs = require('yargs')
+
+
+
+yargs.command({
+    command:'add',
+    describe:'add 2 number',
+    handler: function (args) {
+        console.log(args.num1 + args.num2)
+    }
+})
+yargs.command({
+    command:'sub',
+    describe:'add 2 number',
+    handler: function (args) {
+        console.log(args.num1 - args.num2)
+    }
+})
+yargs.command({
+    command:'mult',
+    describe:'add 2 number',
+    handler: function (args) {
+        console.log(args.num1 * args.num2)
+    }
+})
+yargs.command({
+    command:'pow',
+    describe:'add 2 number',
+    handler: function (args) {
+        console.log(args.num1 + args.num2)
+    }
+})
+
+
+
+yargs.argv
+
